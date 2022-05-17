49349f99099feeace1605d7e3def14366d44f54a
Carlos Lovera <carloslovera1314@gmail.com>
2022-05-17 20:52:04 -0300

    segundo commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,128 @@
-/*const contenido = document.getElementById ("contenido");
+"use strict";
+//alert("Hola mundo");
+/*alert("Hola programadores");
 
-contenido.innerHTML = "contenido";
+alert("Ahora si no abra error");
+[1,2].forEach(alert)
 
-function name(params) {
-    prompt()
+let mensaje = "";
+mensaje = "Soy carlos";
+
+alert(mensaje);
+
+let num = 5;
+alert(num);
+
+const COLOR_ORANGE = "#FF77F00";
+
+let color = COLOR_ORANGE;
+
+alert("color");*/
+
+/*let double = "comillas dobles";
+let single = 'comillas simples';
+let backtick = `Hola ${name}`;
+
+alert(double);
+alert(single);
+alert(backtick);*/
+
+/*let name = "carlos";
+alert(`Hola ${name}`);
+//Hola carlos
+
+alert(`El resultado es ${2 + 2}`);*/
+//El resultado es 4
+
+/*let isFriday = true;
+//Es viernes
+
+let isFinishThisClass = false;
+//La clase aun no ha terminado
+
+alert(isFriday);
+alert(isFinishThisClass);*/
+
+
+
+/*let num = Number("esto no es un numero");
+alert(num);
+
+alert( Number (" 567 ") );
+alert( Number ("567z") );
+alert( Number (true) );
+alert( Number (false) );*/
+
+/*let a = "Nuevo" + " string";
+alert(a);*/
+
+/*let x = 1;
+alert( +x );
+
+let y = -2;
+alert( +y );
+
+let c = 3;
+alert( +c );*/
+
+//let a = "6";
+//let b = "4";
+
+//alert( +a + +b );
+//alert( Number(a) + Number(b) );
+
+//let c = 5;
+//c--;
+//alert( c );
+//let test = prompt( "title", "" );
+
+/*let isBoss = confirm("¿Eres tu el jefe?");
+alert( isBoss );*/
+
+/*let name = prompt( "¿Cual es el nombre de tu profesor?" );
+if ( name == 'kiko' ) {
+    alert( "Correcto!!");
+    alert( "Eres lo maximo!!" );
+} else {
+    alert( 'Sigue intentando!!' );
+    alert( 'Sigue estudiando perro loco!!' );
+}*/
+
+//anidando
+/*let year = prompt('¿En que año salio javascrip?', '');
+
+if (year < 2015) {
+    alert( 'Demasiado pronto...' );
+} else if (year > 2015) {
+    alert( 'Te has pasado' );
+} else {
+    alert( 'Correcto!!' );
+}*/
+
+/*let age = prompt( '¿Que edad tienes?' );
+
+if ( age > 18 ) {
+    alert( 'Eres mayor' );
+} else {
+    alert( 'Eres menor, no puedes entrar' );
+}*/
+
+/*let hour = 12;
+let isWeekend = true;
+
+if (hour < 9 || hour > 17 || isWeekend ) {
+    alert( 'La tienda esta cerrada!' );
+}*/
+
+/*let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+    alert( 'La hora es 12:30' );
+}*/
+
+let i = 3;
+while (i) {
+    alert( i );
+    i--;
 }
-
-
-
